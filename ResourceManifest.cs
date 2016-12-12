a9c954219d09ff02ec79d85c8e2c4db6dad06c51
Csanad Balla <csanad.balla@lombiq.com>
2016-12-12 01:20:52 +0100

    Fixing paths in resource manifest
    --HG--
    branch : issue/OSOE-11

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -10,10 +10,10 @@ namespace Lombiq.Layouts
 
             manifest.DefineScript("Lombiq.Layouts.LayoutEditor").SetUrl("LayoutEditor.js").SetDependencies("Layouts.LayoutEditor");
 
-            manifest.DefineStyle("OwlCarouselTheme").SetUrl("../Content/OwlCarousel/owl.theme.css");
+            manifest.DefineStyle("OwlCarouselTheme").SetBasePath(manifest.BasePath + "Content/OwlCarousel/").SetUrl("owl.theme.css");
             manifest.DefineStyle("OwlCarouselCustomTheme").SetUrl("owl-carousel-custom-theme.min.css", "owl-carousel-custom-theme.css").SetDependencies("OwlCarouselTheme");
-            manifest.DefineStyle("OwlCarouselTransitions").SetUrl("../Content/OwlCarousel/owl.transitions.css");
-            manifest.DefineStyle("OwlCarousel").SetUrl("../Content/OwlCarousel/owl.carousel.css").SetDependencies("OwlCarouselCustomTheme", "OwlCarouselTransitions");
+            manifest.DefineStyle("OwlCarouselTransitions").SetBasePath(manifest.BasePath + "Content/OwlCarousel/").SetUrl("owl.transitions.css");
+            manifest.DefineStyle("OwlCarousel").SetBasePath(manifest.BasePath + "Content/OwlCarousel/").SetUrl("owl.carousel.css").SetDependencies("OwlCarouselCustomTheme", "OwlCarouselTransitions");
             manifest.DefineScript("OwlCarousel").SetUrl("owl.carousel.min.js", "owl.carousel.js").SetDependencies("jQuery");
         }
     }
